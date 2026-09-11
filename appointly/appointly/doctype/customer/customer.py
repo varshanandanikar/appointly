7ac74fcf3a848e4c1db6791214d1310c2a1edd8d
Varsha Nandanikar <varshanandanikar2@gmail.com>
2026-09-11 18:20:40 +0530

    Add phone (10-digit) and email validation, both client-side and server-side

--- appointly/appointly/doctype/customer/customer.py
+++ appointly/appointly/doctype/customer/customer.py
@@ -1,8 +1,13 @@
 # Copyright (c) 2026, Varsha Nandanikar and contributors
 # For license information, please see license.txt
 
-# import frappe
+import re
+
+import frappe
+from frappe import _
 from frappe.model.document import Document
+
+PHONE_RE = re.compile(r"^[6-9]\d{9}$")
 
 
 class Customer(Document):
@@ -20,3 +25,12 @@ class Customer(Document):
 	# end: auto-generated types
 
 	_DOCTYPE_NAME = "Customer"
+
+	def validate(self):
+		self.validate_phone()
+
+	def validate_phone(self):
+		phone = (self.phone or "").strip()
+		if not PHONE_RE.match(phone):
+			frappe.throw(_("Enter a valid 10-digit phone number."))
+		self.phone = phone
